fd1b56cbebe624d29045a5320f959ab2720fee49
Ashwini <ashwini.kr.ranjan98@gmail.com>
2026-04-20 00:26:27 +0530

    Configure GitHub Pages deployment

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -1,3 +1,4 @@
+import { DOCUMENT } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { map, shareReplay } from 'rxjs';
@@ -8,13 +9,16 @@ import { BlogPost, Project } from '../models/content.models';
 })
 export class ContentService {
   private readonly http = inject(HttpClient);
+  private readonly document = inject(DOCUMENT);
+  private readonly projectsUrl = new URL('data/projects.json', this.document.baseURI).toString();
+  private readonly blogPostsUrl = new URL('data/blog-posts.json', this.document.baseURI).toString();
 
   private readonly projects$ = this.http
-    .get<Project[]>('/data/projects.json')
+    .get<Project[]>(this.projectsUrl)
     .pipe(shareReplay(1));
 
   private readonly blogPosts$ = this.http
-    .get<BlogPost[]>('/data/blog-posts.json')
+    .get<BlogPost[]>(this.blogPostsUrl)
     .pipe(
       map((posts) =>
         [...posts].sort(
--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -16,6 +16,7 @@ export class SeoService {
   private readonly title = inject(Title);
   private readonly meta = inject(Meta);
   private readonly document = inject(DOCUMENT);
+  private readonly defaultImage = new URL('assets/og-cover.svg', this.document.baseURI).toString();
 
   public update(meta: PageMeta) {
     this.title.setTitle(meta.title);
@@ -23,9 +24,9 @@ export class SeoService {
     this.meta.updateTag({ property: 'og:title', content: meta.title });
     this.meta.updateTag({ property: 'og:description', content: meta.description });
     this.meta.updateTag({ property: 'og:url', content: meta.url ?? this.document.location.href });
-    this.meta.updateTag({ property: 'og:image', content: meta.image ?? '/assets/og-cover.svg' });
+    this.meta.updateTag({ property: 'og:image', content: meta.image ?? this.defaultImage });
     this.meta.updateTag({ name: 'twitter:title', content: meta.title });
     this.meta.updateTag({ name: 'twitter:description', content: meta.description });
-    this.meta.updateTag({ name: 'twitter:image', content: meta.image ?? '/assets/og-cover.svg' });
+    this.meta.updateTag({ name: 'twitter:image', content: meta.image ?? this.defaultImage });
   }
 }
